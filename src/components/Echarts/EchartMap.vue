edc7cd66cd87315af19eeabc311d603ec3cbcc1d
飞鱼 <1252331215@qq.com>
2020-04-03 17:49:59 +0800

    初始化echarts画图

---
+++ src/components/Echarts/EchartMap.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="main" style="width:1000px;height:800px"></div>
+</template>
+
+<script>
+  import echarts from 'echarts'
+  export default {
+    name: "EchartMap",
+    data(){
+      return{
+        options:{
+          title: {
+            text: 'ECharts 入门示例'
+          },
+          tooltip: {},
+          legend: {
+            data:['销量']
+          },
+          xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+          },
+          yAxis: {},
+          series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
+        }
+      }
+    },
+    methods:{
+    },
+    created() {
+    },
+    mounted() {
+      let MyChart = echarts.init(document.querySelector('#main'),null,{renderer:'svg'})
+      console.log(MyChart);
+      console.log(this.options)
+      MyChart.setOption(this.options)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li><router-link to="/echarts">Echarts</router-link></li>
+      <li><router-link to="/G2">G2</router-link></li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
